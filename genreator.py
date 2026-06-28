a750acbab1c29edec29fb96ee6f99039c1db08c7
Artieur262 <raguinmateo@gmail.com>
2026-06-28 16:13:25 +0200

    pront +

--- genreator.py
+++ genreator.py
@@ -5,4 +5,5 @@ Le programme lit ce dossier pour afficher les skins disponibles.
 Quand on sélectionne un skin, on doit avoir un aperçu du skin avant de l'appliquer.
 L'aperçu contient le nom du skin, une image du skin et la taille du fichier.
 Les images de preview sont stockées dans un dossier "apercus" et le programme crée une image du skin ressemblant à une photo.
+pour une photo il doit faire un rendu du skin avec un fond blanc et une lumière douce, le skin doit être centré et bien visible.
 """
