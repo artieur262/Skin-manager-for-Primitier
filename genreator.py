2753b47c5356ce29f4bba112fcbec77640fc7b84
Artieur262 <raguinmateo@gmail.com>
2026-06-28 15:49:35 +0200

    pront

---
+++ genreator.py
@@ -0,0 +1,9 @@
+"""
+Ce programme a pour but de géner les images de preview pour les skins VRM. 
+La liste des skins disponibles est stockée dans un dossier "skins" 
+et le programme va lire ce dossier pour afficher les skins disponibles. 
+Quand on sélectionne un skin, on doit avoir un aperçu du skin avant de l'appliquer. 
+L'aperçu doit avoir le nom du skin, une image du skin et la taille du fichier.
+les images de preview sont stockées dans un dossier "apercus" et le programme va lire ce dossier pour afficher les aperçus disponibles.
+"""
+
